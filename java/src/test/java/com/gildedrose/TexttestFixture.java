26ebfe3b11a530cc076d634fa5cd4bc0f401a324
Christian <wiedehoeft.christian@gmail.com>
2019-02-05 09:45:28 +0100

    Fix package structure

--- java/src/test/java/com/gildedrose/TexttestFixture.java
+++ java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -6,7 +6,9 @@ public class TexttestFixture {
 
         Item[] items = new Item[]{
                 new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("+5 Dexterity Vest", 0, 20), //
                 new Item("Aged Brie", 2, 0), //
+                new Item("Aged Brie", 0, 8), //
                 new Item("Elixir of the Mongoose", 5, 7), //
                 new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
